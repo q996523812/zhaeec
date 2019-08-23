a3a20fd5137ac152e31da16d1482582e30482b81
qss <doublesan@126.com>
2019-08-23 03:23:09 +0000

    修改接口地址为对方测试环境地址

--- app/Services/WbjkProjectBaseService.php
+++ app/Services/WbjkProjectBaseService.php
@@ -23,10 +23,10 @@ class WbjkProjectBaseService
     private $fields_projectt = ['xmbh','title','price','gp_date_start','gp_date_end'];
     // protected $IP_TEST = '47.112.15.51';
     // protected $PORT_TEST = '8090';
-    // protected $IP = '47.112.15.51';
-    // protected $PORT = '8090';
-    protected $IP = '172.20.10.3';
-    protected $PORT = '8088';
+    protected $IP = '47.112.15.51';
+    protected $PORT = '8090';
+    // protected $IP = '172.20.10.3';
+    // protected $PORT = '8088';
 
     /**
      * 根据字段列表，从接口数据表中获取业务数据
