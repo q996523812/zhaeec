8fdcfd0247c849aec6ee9b79fa8b0fa8d3d092c5
qss <doublesan@126.com>
2019-11-06 12:17:48 +0000

    添加更多日志

--- app/Handlers/CurlHandler.php
+++ app/Handlers/CurlHandler.php
@@ -17,31 +17,30 @@ class CurlHandler
      */	
 	public static function curl($url, $params = false, $ispost = 1, $https = 0)
     {
-        Log::info('curl begin');
         $httpInfo = array();
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36');
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         if ($https) {
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // 对认证证书来源的检查
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); // 从证书中检查SSL加密算法是否存在
         }
-        Log::info('111');
-        if ($ispost) {Log::info('333');Log::info($params);
+        
+        if ($ispost) {
             if ($params) {
                 if (is_array($params)) {
                     $params = json_encode($params);
                 }
             }
-            Log::info('555');Log::info($params);
+
             curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
             curl_setopt($ch, CURLOPT_URL, $url);
-        } else {Log::info('444');
+        } else {
             if ($params) {
                 if (is_array($params)) {
                     $params = http_build_query($params);
@@ -51,27 +50,18 @@ class CurlHandler
                 curl_setopt($ch, CURLOPT_URL, $url);
             }
         }
-        Log::info('222');
+        
         $response = curl_exec($ch);
-        Log::info('response');
-        Log::info($response);
 
         if ($response === FALSE) {
             //echo "cURL Error: " . curl_error($ch);
-            Log::info("cURL Error: " . curl_error($ch));
-            Log::info('666');
+            Log::info("CURL Error: " . curl_error($ch));
             return false;
         }
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $httpInfo = array_merge($httpInfo, curl_getinfo($ch));
         curl_close($ch);
 
-        
-        Log::info('httpCode');
-        Log::info($httpCode);
-        Log::info('httpInfo');
-        Log::info($httpInfo);
-        Log::info('curl end');
         return $response;
     }
 
