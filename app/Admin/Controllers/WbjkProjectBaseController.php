399f6208c889bbcd73ea5e25a76bc518acf7954e
qss <doublesan@126.com>
2019-11-06 11:34:37 +0000

    缺少引用

--- app/Admin/Controllers/WbjkProjectBaseController.php
+++ app/Admin/Controllers/WbjkProjectBaseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Controllers;
 
+use App\Models\Project;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\HasResourceActions;
 use Encore\Admin\Form;
@@ -171,7 +172,7 @@ class WbjkProjectBaseController extends Controller
                     break;
                 case 7://已接收，等待自动发送挂牌数据
                     $bottons = $getBotton('发送挂牌数据','发送挂牌数据','edit2',$rec->id,'sendGp');
-                    
+
                     break;
                 /*******业务公共部分*********/
                 case 119://自动发送挂牌数据失败，等待手工发送，下同
