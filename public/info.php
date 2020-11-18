a48cf4e18ddaae38a003c0bd2e76f08a25e7986a
qss <doublesan@126.com>
2020-11-18 09:08:53 +0000

    测试联合产权接口详情页面访问

--- public/info.php
+++ public/info.php
@@ -1,1 +1,16 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Document</title>
+ </head>
+ <body>
+  <a href="https://www.gduaee.com/portal/pro/index-notop.jsp?proId=ac44e9365adb4a93984be16d0a4e415a" target="_blank">佛山市顺德区顺大酒店有限公司100%股权</a>
+ </body>
+</html>
+
 <?php phpinfo(); ?>
